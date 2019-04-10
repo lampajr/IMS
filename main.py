320e6207ad41ad0f83fc8a9fbca542b79ed22440
lampa <lampa9559@gmail.com>
2019-04-10 07:19:19 +0200

    some problem fixed

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ from monitor import Monitor
 
 if __name__ == '__main__':
     filename = ""
+    use_clear = None
     try:
         if len(sys.argv) > 1:
             filename = sys.argv[1]
+            if len(sys.argv) > 2:
+                use_clear = sys.argv[2].lower() == "true"
         else:
-            filename = input("Insert file's path .. ")
+            filename = input("Insert file's path.. ")
+            use_clear = input("Use terminal clear..").lower() == "true"
+
+        use_clear = True if use_clear is None else use_clear
 
         loader = Loader(filename=filename)
-        Monitor(tasks=loader.tasks, agents=loader.agents, clear=True, refresh_rate=loader.refresh_rate).start()
+        Monitor(tasks=loader.tasks, agents=loader.agents, use_clear=use_clear, refresh_rate=loader.refresh_rate).start()
 
         for t in loader.starter_tasks:
             try:
--- monitor.py
+++ monitor.py
@@ -29,24 +29,25 @@ class Monitor(threading.Thread):
     BASE_TASK = "TASKS"
     BASE_PROGRESS = "PROGRESS"
 
-    def __init__(self, tasks, agents, clear=True, refresh_rate=1):
+    def __init__(self, tasks, agents, use_clear=True, refresh_rate=1):
         super(Monitor, self).__init__()
         self.tasks = tasks
         self.agents = agents
         self.refresh_rate = refresh_rate  # in seconds
-        self.clear= clear
+        self.use_clear = use_clear
+        self.count = 0
 
         self.name_width = 30
         self.state_width = 30
         self.description_width = 30
         self.topic_width = 30
 
-        self.border = 170 * "*"
-        self.header = "**{0: ^30s}**{1: ^30s}**{2: ^70s}**{3: ^30s}**".format(Monitor.BASE_NAME,
+        self.border = 170 * "="
+        self.header = "||{0: ^30s}||{1: ^30s}||{2: ^70s}||{3: ^30s}||".format(Monitor.BASE_NAME,
                                                                               Monitor.BASE_STATE,
                                                                               Monitor.BASE_DESCRIPTION,
                                                                               Monitor.BASE_TOPIC)
-        self.task_header = "**{0: ^30s}**{1: ^30s}**{2: ^70s}**{3: ^30s}**".format(Monitor.BASE_TASK,
+        self.task_header = "||{0: ^30s}||{1: ^30s}||{2: ^70s}||{3: ^30s}||".format(Monitor.BASE_TASK,
                                                                                    Monitor.BASE_STATE,
                                                                                    Monitor.BASE_PROGRESS,
                                                                                    Monitor.BASE_TOPIC)
@@ -57,15 +58,18 @@ class Monitor(threading.Thread):
                 return False
         return True
 
-    # define our clear function
-
     def __print_monitor(self):
 
         """ print the state of all the agents involved in the system
             and of all the tasks executed and that have to be executed """
 
-        if self.clear:
+        if self.use_clear:
             clear()
+        else:
+            print("\n"*15)
+            print("step {}".format(self.count))
+            self.count += 1
+
         # Agents' monitor
         print(self.border)
         print(self.header)
@@ -74,22 +78,25 @@ class Monitor(threading.Thread):
         for a in self.agents:
             if a.topic != topic:
                 topic = a.topic
-                print("**" + "-"*166 + "**")
+                print("||" + "-"*166 + "||")
             state = colored("executing", color="yellow") if a.executing else colored("occupied", color="blue") \
                 if a.occupied else colored("failed", color="red") if a.failed else colored("free", color="green")
             description = a.current_task.logger.description if a.executing else "offering " + colored(a.bid, "red") + " in {} auction".format(a.current_task.logger.name) if a.occupied else "empty"
             if not a.executing and a.occupied:
-                line = "**{0: ^30s}**{1: ^39s}**{2: ^79s}**{3: ^30}**".format(a.logger.name,
+                line = "||{0: ^30s}||{1: ^39s}||{2: ^79s}||{3: ^30}||".format(a.logger.name,
                                                                               state,
                                                                               description,
                                                                               a.topic.value)
             else:
-                line = "**{0: ^30s}**{1: ^39s}**{2: ^70s}**{3: ^30}**".format(a.logger.name,
+                line = "||{0: ^30s}||{1: ^39s}||{2: ^70s}||{3: ^30}||".format(a.logger.name,
                                                                               state,
                                                                               description,
                                                                               a.topic.value)
             cprint(line)
         print(self.border)
+
+        # spaces
+        print("\n\n")
 
         # Tasks' monitor
         print(self.border)
@@ -98,13 +105,13 @@ class Monitor(threading.Thread):
         count = 0
         for t in self.tasks:
             if count == 4:
-                print("**" + "-"*166 + "**")
+                print("||" + "-"*166 + "||")
                 count = 0
             state = colored("terminated!", "red") if t.terminated \
                 else colored("allocated", "green") if t.allocated else colored("to be allocated..", "blue") \
                 if t.generated else colored("not yet generated", "grey")
             progress = "{}% complete..".format(t.percentage) if t.progress != 0 else "not yet started!"
-            line = "**{0: ^30s}**{1: ^39s}**{2: ^70s}**{3: ^30s}**".format(t.logger.name, state, progress, get_topic_from_subjects(t.subjects).value)
+            line = "||{0: ^30s}||{1: ^39s}||{2: ^70s}||{3: ^30s}||".format(t.logger.name, state, progress, get_topic_from_subjects(t.subjects).value)
             cprint(line)
             count += 1
         print(self.border)
@@ -113,7 +120,6 @@ class Monitor(threading.Thread):
         started_time = get_time()
         while not self.__check_termination():
             self.__print_monitor()
-            #print("\n\n\n\n\n\n\n\n")
             print("")
             time.sleep(self.refresh_rate)
         for a in self.agents:
